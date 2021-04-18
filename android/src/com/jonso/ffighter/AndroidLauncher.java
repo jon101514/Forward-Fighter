7152bafafeaab8b12da401044c9f31427cff3277
Jonathan So <jonso.gamedev@gmail.com>
2021-04-17 21:02:56 -0400

    Initialized libGDX project.

---
+++ android/src/com/jonso/ffighter/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.jonso.ffighter;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.jonso.ffighter.FFighter;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FFighter(), config);
+	}
+}
